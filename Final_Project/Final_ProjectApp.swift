3ca1b0dbad8c020b9607958fca3f2a7d49f1494f
Yuhui <00857104@email.ntou.edu.tw>
2022-01-25 13:17:16 +0800

    Initial Commit

---
+++ Final_Project/Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Final_ProjectApp.swift
+//  Final_Project
+//
+//  Created by Len on 2022/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
